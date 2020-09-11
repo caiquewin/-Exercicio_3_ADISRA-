f576472815b67e44419a6b96634d86654f94364d
Caique Pereira Alves <caiquewin@gmail.com>
2020-09-11 17:00:58 -0300

    Projeto criado e finalizado

---
+++ Exercicio 3/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Exercicio_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo4 = new System.Windows.Forms.Label();
+            this.BtnLimpar = new System.Windows.Forms.Button();
+            this.picImagem = new System.Windows.Forms.PictureBox();
+            this.BtnSair = new System.Windows.Forms.Button();
+            this.lblTitulo3 = new System.Windows.Forms.Label();
+            this.listCaixa = new System.Windows.Forms.ListBox();
+            this.textCodigo = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblProduto = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitulo4
+            // 
+            this.lblTitulo4.AutoSize = true;
+            this.lblTitulo4.Font = new System.Drawing.Font("Yu Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo4.ForeColor = System.Drawing.Color.Tomato;
+            this.lblTitulo4.Location = new System.Drawing.Point(12, 281);
+            this.lblTitulo4.Name = "lblTitulo4";
+            this.lblTitulo4.Size = new System.Drawing.Size(158, 26);
+            this.lblTitulo4.TabIndex = 25;
+            this.lblTitulo4.Text = "VALOR TOTAL:";
+            // 
+            // BtnLimpar
+            // 
+            this.BtnLimpar.BackColor = System.Drawing.Color.PowderBlue;
+            this.BtnLimpar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLimpar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BtnLimpar.Location = new System.Drawing.Point(522, 282);
+            this.BtnLimpar.Name = "BtnLimpar";
+            this.BtnLimpar.Size = new System.Drawing.Size(86, 27);
+            this.BtnLimpar.TabIndex = 24;
+            this.BtnLimpar.Text = "Limpar";
+            this.BtnLimpar.UseVisualStyleBackColor = false;
+            this.BtnLimpar.Click += new System.EventHandler(this.BtnLimpar_Click);
+            // 
+            // picImagem
+            // 
+            this.picImagem.Location = new System.Drawing.Point(25, 109);
+            this.picImagem.Name = "picImagem";
+            this.picImagem.Size = new System.Drawing.Size(150, 124);
+            this.picImagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picImagem.TabIndex = 23;
+            this.picImagem.TabStop = false;
+            // 
+            // BtnSair
+            // 
+            this.BtnSair.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BtnSair.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSair.ForeColor = System.Drawing.Color.Firebrick;
+            this.BtnSair.Location = new System.Drawing.Point(522, 245);
+            this.BtnSair.Name = "BtnSair";
+            this.BtnSair.Size = new System.Drawing.Size(86, 29);
+            this.BtnSair.TabIndex = 22;
+            this.BtnSair.Text = "Sair";
+            this.BtnSair.UseVisualStyleBackColor = false;
+            this.BtnSair.Click += new System.EventHandler(this.BtnSair_Click);
+            // 
+            // lblTitulo3
+            // 
+            this.lblTitulo3.AutoSize = true;
+            this.lblTitulo3.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitulo3.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo3.Location = new System.Drawing.Point(189, 281);
+            this.lblTitulo3.Name = "lblTitulo3";
+            this.lblTitulo3.Size = new System.Drawing.Size(0, 17);
+            this.lblTitulo3.TabIndex = 21;
+            // 
+            // listCaixa
+            // 
+            this.listCaixa.FormattingEnabled = true;
+            this.listCaixa.Location = new System.Drawing.Point(192, 72);
+            this.listCaixa.Name = "listCaixa";
+            this.listCaixa.Size = new System.Drawing.Size(185, 173);
+            this.listCaixa.TabIndex = 20;
+            // 
+            // textCodigo
+            // 
+            this.textCodigo.Location = new System.Drawing.Point(25, 72);
+            this.textCodigo.Name = "textCodigo";
+            this.textCodigo.Size = new System.Drawing.Size(150, 20);
+            this.textCodigo.TabIndex = 19;
+            this.textCodigo.TextChanged += new System.EventHandler(this.textCodigo_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Kristen ITC", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(394, 225);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(143, 22);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "006   -    Maionese";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Kristen ITC", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(394, 192);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(121, 22);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "005   -    Salada";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Kristen ITC", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(394, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(166, 22);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "004   -    Refrigerante";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Kristen ITC", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(394, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 22);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "003   -    Batata Frita";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Kristen ITC", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(394, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(231, 22);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "002   -    Hamburge De Frango";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Kristen ITC", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(394, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 22);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "001   -    Hamburge De Carne";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblProduto
+            // 
+            this.lblProduto.AutoSize = true;
+            this.lblProduto.BackColor = System.Drawing.Color.Transparent;
+            this.lblProduto.Font = new System.Drawing.Font("Yu Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProduto.ForeColor = System.Drawing.Color.Black;
+            this.lblProduto.Location = new System.Drawing.Point(25, 49);
+            this.lblProduto.Name = "lblProduto";
+            this.lblProduto.Size = new System.Drawing.Size(182, 19);
+            this.lblProduto.TabIndex = 18;
+            this.lblProduto.Text = "CÓDIGO DO PRODUTO:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(385, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Código";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(485, 40);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(75, 20);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Descrição";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 326);
+            this.Controls.Add(this.lblTitulo4);
+            this.Controls.Add(this.BtnLimpar);
+            this.Controls.Add(this.picImagem);
+            this.Controls.Add(this.BtnSair);
+            this.Controls.Add(this.lblTitulo3);
+            this.Controls.Add(this.listCaixa);
+            this.Controls.Add(this.textCodigo);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblProduto);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo4;
+        private System.Windows.Forms.Button BtnLimpar;
+        private System.Windows.Forms.PictureBox picImagem;
+        private System.Windows.Forms.Button BtnSair;
+        private System.Windows.Forms.Label lblTitulo3;
+        private System.Windows.Forms.ListBox listCaixa;
+        private System.Windows.Forms.TextBox textCodigo;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblProduto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Exercicio 3/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace Exercicio_3
+{
+    public partial class Form1 : Form
+    {
+        string[] Produtos = new string[10];
+        string[] Codigo = new string[10];
+        double[] Valor = new double[10];
+        double soma = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CarregarArray();
+            soma = 0;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnLimpar_Click(object sender, EventArgs e)
+        {
+            listCaixa.Items.Clear();
+            picImagem.Image = null;
+            lblTitulo3.Text = " ";
+        }
+
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void textCodigo_TextChanged(object sender, EventArgs e)
+        {
+
+            if (textCodigo.Text.Length == 3)
+
+
+            {
+                int indice = 0;
+                for (int prod = 1; prod < Codigo.Length; prod++)
+
+                {
+                    if (textCodigo.Text == Codigo[prod])
+                    {
+                        indice = prod;
+
+                    }
+                }
+
+                if (indice == 0)
+                {
+                    MessageBox.Show("Enrisa código ao lado :D");
+                }
+
+                /*Condição, se foi encontrado o produto ele retorna as informações de acordo com o índice 
+                  caso não encontre aparecerá a mensagem, Produto não encontrado*/
+
+                else
+                {
+                    listCaixa.Items.Add(textCodigo.Text + " " + Produtos[indice] + " R$ " + Valor[indice].ToString("f2",CultureInfo.InvariantCulture));
+
+
+                    soma = soma + Valor[indice];
+                    lblTitulo3.Text = ("R$: " + soma.ToString("f2",CultureInfo.InvariantCulture));
+                    //local da imagem
+                    picImagem.ImageLocation = "C:/Users/caique/source/repos/Exercicio 3/Exercicio 3/Imagens/" + Codigo[indice] + ".jpg";
+                    textCodigo.Text = "";
+                    textCodigo.Focus();
+                }
+
+            }
+
+        }
+        
+        private void CarregarArray()
+        {
+            /*Criação do Método para ser chamado quando necessário, 
+             isso evita repetir várias vezes o mesmo código*/
+
+            Codigo[1] = "001";
+            Codigo[2] = "002";
+            Codigo[3] = "003";
+            Codigo[4] = "004";
+            Codigo[5] = "005";
+            Codigo[6] = "006";
+
+            Produtos[1] = "Hamburge De Carne";
+            Produtos[2] = "Hamburge De Frango";
+            Produtos[3] = "Batata Frita";
+            Produtos[4] = "Refrigerante";
+            Produtos[5] = "Salada";
+            Produtos[6] = "Maionese";
+
+            Valor[1] = 20.00;
+            Valor[2] = 16.00;
+            Valor[3] = 6.00;
+            Valor[4] = 4.00;
+            Valor[5] = 2.00;
+            Valor[6] = 1.00;
+        }
+
+    }
+}
